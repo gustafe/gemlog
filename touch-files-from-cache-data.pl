b82cbfb19ad80b6555212de1b4efec4b6f119c79
Gustaf Erikson <gerikson@gmail.com>
2021-10-05 21:42:10 +0200

    added binaries

---
+++ touch-files-from-cache-data.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+my $indexfile = '/home/gustaf/gemini/gemlog-plugins/state/.entries_cache.index';
+
+# load the timestamps
+my %timestamps = ();
+open CACHE, "<$indexfile" or die "couldn't open $indexfile for reading: $!";
+while ( <CACHE> ) {
+    chomp;
+    my ( $filename, $timestamp ) = split( /\=\>/, $_ );
+#    print "$_ $filename $timestamp\n";
+    $timestamps{$filename} = $timestamp;
+}
+close CACHE;
+
+foreach my $filename ( keys %timestamps ) {
+    my $timestamp = $timestamps{$filename};
+    my $rv = utime( $timestamp, $timestamp, $filename );
+    warn "failed to update timestamp of $filename to $timestamp" if $rv != 1;
+}
